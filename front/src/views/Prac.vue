bdbe8db1ec171895534d14b03aaf1e41a2d91a31
임승현 <limseung10@gmail.com>
2020-09-09 15:57:51 +0900

    Chore (F) | 노트 모듈 삭제 테스트

---
+++ front/src/views/Prac.vue
@@ -0,0 +1,24 @@
+<template>
+  <VueCompareImage :leftImage="require('@/assets/1-1.jpg')" :rightImage="require('@/assets/1-2.jpg')" />
+
+</template>
+
+<script>
+import VueCompareImage from 'vue-compare-image';
+
+export default {
+  name: 'Prac',
+  components: {
+    VueCompareImage 
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
