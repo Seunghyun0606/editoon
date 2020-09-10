891bb1a0cbe158b9e67449101bde7b572947acce
dkdlrnf0 <dkdlrnf0@gmail.com>
2020-09-10 14:17:24 +0900

    Feat(B)|Start Project with SpringBoot

---
+++ editoon/src/test/java/com/nokk/editoon/EditoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokk.editoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
