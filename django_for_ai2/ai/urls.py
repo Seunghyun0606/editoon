736915d66cb252b26b0cc81e366b9e209cb44904
smile_666 <smile_666@naver.com>
2020-09-26 23:47:45 +0900

    Feat(A) | delete folder

--- django_for_ai2/ai/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'ai'
-
-urlpatterns = [
-    path('ImgtoAnime/', views.ImgtoAnime),
-
-]   
--- django_for_ai2/test.py
+++
@@ -1,7 +0,0 @@
-from glob import glob
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-file_paths = glob('output/UGATIT*/*.jpg')
-print(file_paths)
-print(BASE_DIR)
