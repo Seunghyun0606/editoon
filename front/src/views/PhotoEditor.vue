7cfcb866997f657eec82ebd27f8c4dbe7dd3f124
임승현 <limseung10@gmail.com>
2020-09-28 03:44:45 +0900

    Feat (F) | 배경 그라디언트 옵션 추가

--- front/src/views/PhotoEditor.vue
+++ front/src/views/PhotoEditor.vue
@@ -31,24 +31,21 @@
             <!-- <p v-if="image.isBubble" >abcdefawefawefawefawefawefaabcdefawefawefawefawefawefaabcdefawefawefawefawefawefaabcdefawefawefawefawefawefa</p> -->
 
             <!-- background의 경우 -->
-            <div v-if="image.isClickOption && image.isBackground" style="width: 500px; height: 500px; background-color: red; position: relative; z-index: 999; left: calc(100% + 50px);">
-              <div>
-                상단 테두리 border정도
-              </div>
-              <div>
-                상단 테두리 opacity 정도
-              </div>
-              <div>
-                하단 테두리 border정도
-              </div>
-              <div>
-                하단 테두리 opacity 정도
-              </div>
+            <div v-if="image.isClickOption && image.isBackground" style="width: 500px; height: 500px; background-color: black; position: relative; z-index: 999; left: calc(100% + 50px);">
 
+              <v-btn @click="image.backgroundOption.gradientCheck = 0">
+                일반
+              </v-btn>
+              <v-btn @click="image.backgroundOption.gradientCheck = 1">
+                회상 윗배경
+              </v-btn>
+              <v-btn @click="image.backgroundOption.gradientCheck = 2">
+                회상 아랫배경
+              </v-btn>
 
             </div>
             <!-- 말풍선의 경우 -->
-            <div v-if="image.isClickOption && image.isBubble" style="width: 500px; height: 500px; background-color: blue; position: relative; z-index: 999; left: calc(100% + 50px);">
+            <div v-if="image.isClickOption && image.isBubble" style="width: 500px; height: 500px; background-color: black; position: relative; z-index: 999; left: calc(100% + 50px);">
               <v-container>
                 <v-row>
                   <v-col>
@@ -168,17 +165,17 @@ export default {
         {
           image: require(`@/assets/account_signup.png`),  // 맨처음 테스트용으로 넣은것
           isActive: false,  // 나중에 중복 선택 제거를 위함.
-          isBackground: false, // 배경인지 확인하기위함.
+          isBackground: true, // 배경인지 확인하기위함.
           isBubble: false,  // 말풍선인지 확인
           zIndex: 102,
           isClickOption: false,
-          isDraggable: false,
+          isDraggable: true,
           imageOption: {
             borderSlider: 5,
             borderColor: '#000',
           },
           backgroundOption: {
-
+            gradientCheck: 0  // 0 없음, 1 upper, 2 lower
           },
           bubbleOption: {
 
@@ -208,7 +205,16 @@ export default {
         let imageStyle = {}
 
         if ( image.isBackground ) {
-          imageStyle.backgroundColor = 'black'
+          let gradientCheck = image.backgroundOption.gradientCheck
+          if ( gradientCheck === 1 ) {
+            imageStyle.background = "linear-gradient( white , black )"
+          }
+          else if ( gradientCheck === 2) {
+            imageStyle.background = "linear-gradient( black , white )"
+          }
+          else {
+            imageStyle.backgroundColor = 'black'
+          }
         }
         else if ( image.isBubble ) {
           imageStyle.backgroundColor = 'blue'
@@ -222,17 +228,6 @@ export default {
         }
         return imageStyle
       },
-      addBackground: {
-        backgroundColor: 'black',
-      },
-      addImage: function(imageURL) {
-        let imageStyle = {
-          backgroundImage:  'url('+ `${imageURL}` + ')',
-          backgroundRepeat: 'round' 
-        }
-        return imageStyle
-      },
-
       useDefaultUI: true,
       webtoonCanvasHeight: window.innerHeight*0.87,
       webtoonCanvasWidth: 0,
