9e4e667a490178ef11d37bfd31fc2b4be804e5da
임승현 <limseung10@gmail.com>
2020-10-04 22:21:12 +0900

    Fix (F) | 요청주소 변경

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -252,7 +252,7 @@ export default new Vuex.Store({
     },
     // 유저가 저장한 editoon image 목록(썸네일) 보여주기
     getUserEditoonThumbnails({ state, commit }) {
-      axios.get( SERVER_URL + 'v1/getEditoonThumbnails', state.userInfo.email, {
+      axios.get( SERVER_URL + 'editoon/v1/getEditoonThumbnails', state.userInfo.email, {
         headers: {
           email: state.userInfo.email
         }
@@ -270,7 +270,7 @@ export default new Vuex.Store({
 
     // 유저가 저장한 editton image 보여주기
     getUserEditoonImages({ state, commit }, book_id) {
-      axios.get( SERVER_URL + 'v1/getEditoonDetail', { email: state.userInfo.email, _id: book_id }, {
+      axios.get( SERVER_URL + 'editoon/v1/getEditoonDetail', { email: state.userInfo.email, _id: book_id }, {
         headers: {
           email: state.userInfo.email
         }
