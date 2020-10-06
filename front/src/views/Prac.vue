d21d4df9d99ac8540c1e9297d35acaa93cf59483
smile_666 <smile_666@naver.com>
2020-10-06 22:40:28 +0900

    Test(A) | test

--- front/src/views/Prac.vue
+++ front/src/views/Prac.vue
@@ -43,7 +43,7 @@ export default {
       formData.append("img2", file.files[1]);
 
       axios
-        .post("http://j3b308.p.ssafy.io:8008/ai/ImgtoAnime/", formData, {
+        .post("http://j3b308.p.ssafy.io/ai/ImgtoAnime/", formData, {
           headers: {
             "Content-Type": "multipart/form-data",
           },
